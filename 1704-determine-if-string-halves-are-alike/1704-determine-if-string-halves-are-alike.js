07fbac5ea81f1a8b54a67bad700229a6160f8106
Sid Ahmed Sahraoui <s.sahraoui@esi-sba.dz>
2022-12-07 14:52:11 +0100

    Time: 106 ms (44.67%), Space: 44.7 MB (15.42%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
@@ -3,15 +3,14 @@
  * @return {boolean}
  */
 var halvesAreAlike = function(s) {
-        let a = s.substr(0,s.length/2)
+    let a = s.substr(0,s.length/2)
     let b = s.substr(s.length/2)
     let ai = 0;
     let bj = 0;
     for(let i=0; i<= a.length; i++) {
         if(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'].includes(a[i]) ) ai++;
-    }
-    for(let j=0; j<= b.length; j++) {
-        if(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'].includes(b[j]) ) bj++;
+        if(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'].includes(b[i]) ) bj++;
+
     }
     if (ai === bj) return true;
     return false;
