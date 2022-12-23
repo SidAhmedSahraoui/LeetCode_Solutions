d11cecb9fb8b745c46a5bbc44487c8121c61a38f
Sid Ahmed Sahraoui <s.sahraoui@esi-sba.dz>
2022-12-23 18:21:50 +0100

    Time: 88 ms (84.52%), Space: 46.4 MB (34.44%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -2,27 +2,24 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function oneSetValidSudoku(board) {
-    let set = new Set()
-    
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[0].length; j++) {
-            const value = board[i][j]
-            if (value !== ".") {
-                const row = `${value} at row ${i}`
-                const column = `${value} at column ${j}`
-                const box = `${value} at box ${Math.floor(i/3)}, ${Math.floor(j/3)}`
-                console.log(`${row}, ${column}, ${box}`)
-                if (set.has(row) || set.has(column) || set.has(box)) {
-                    return false
-                } else {
-                    set.add(row)
-                    set.add(column)
-                    set.add(box)
-                }
+var isValidSudoku = function (board) {
+    let row = [...Array(9)].map(() => Array(9).fill(false));
+    let col = [...Array(9)].map(() => Array(9).fill(false));
+    let sub = [...Array(9)].map(() => Array(9).fill(false));
+    for (let i = 0; i < 9; ++i) {
+        for (let j = 0; j < 9; ++j) {
+            const num = board[i][j].charCodeAt() - '1'.charCodeAt();
+            if (num < 0 || num > 9) {
+                continue;
             }
+            const k = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+            if (row[i][num] || col[j][num] || sub[k][num]) {
+                return false;
+            }
+            row[i][num] = true;
+            col[j][num] = true;
+            sub[k][num] = true;
         }
     }
-
-    return true
+    return true;
 };
