219653b5e6bb58af89826e52d0d846abfd44e42b
Sid Ahmed Sahraoui <s.sahraoui@esi-sba.dz>
2022-12-25 11:05:25 +0100

    Time: 92 ms (84.62%), Space: 45.8 MB (16.35%) - LeetHub

---
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} queries
+ * @return {number[]}
+ */
+var answerQueries = function(nums, queries) {
+nums.sort((a,b)=>a-b)
+let sum=[]
+let acc=0;
+for(let el of nums){
+    acc +=el;
+    sum.push(acc);
+}
+    let result=[]
+    for(let el of queries){
+        result.push(find(sum,el))
+    }
+    return result
+};
+const find=(arr,target)=>{
+let left=0;
+let right=arr.length-1;
+    if(target>=arr[right]){
+        return arr.length
+    }
+    if(target<arr[left]){
+        return 0;
+    }
+    let mid=Math.floor((right+left)/2)
+while(right>left){
+    if(target<arr[mid]){
+        right=mid-1
+    }else if(target>arr[mid]){
+        left=mid+1
+    }else if(target==arr[mid]){
+        return mid+1
+    }
+      mid=Math.floor((right+left)/2)
+}
+    if(target>arr[mid]){
+        return mid+1
+    }else{
+        return mid
+    }
+}
