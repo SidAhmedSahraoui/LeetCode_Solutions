aa899d0959c92f06125d6267a4dae20cc20f037f
Sid Ahmed Sahraoui <s.sahraoui@esi-sba.dz>
2022-12-26 10:29:45 +0100

    Time: 74 ms (94.95%), Space: 46.3 MB (67.30%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    // Keep track of max distance traveled so far
+    let max = 0;
+    for(let i=0;i<nums.length;i++){
+        // The only time that max < i is when we are at 0 and we cannot move forward
+        if(i>max) return false;
+        // Move the max pointer the maximum 
+        max = Math.max(nums[i]+i,max);
+    }
+    // If we did not get stuck at a 0 then return true
+    return true;
+};
